fd4897db4599359556f5cca472a5c7054834aa90
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2022-01-02 11:03:37 +0900

    first commit

---
+++ src/main/java/com/rdr/paradisechat/ParadiseChatApplication.java
@@ -0,0 +1,13 @@
+package com.rdr.paradisechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadiseChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParadiseChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rdr/paradisechat/ParadiseChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdr.paradisechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadiseChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
